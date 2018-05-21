11dae0c4b34d6771a9fa01f6fff12300f81eedfb
Alexandre Santos <sasle57@gmail.com>
2018-05-21 19:18:16 -0300

    Aula 140

--- BattleTank/Source/BattleTank/Private/TankAimingComponent_.cpp
+++ BattleTank/Source/BattleTank/Private/TankAimingComponent_.cpp
@@ -1,6 +1,8 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "TankAimingComponent_.h"
+#include "Components/StaticMeshComponent.h"
+#include "Kismet/GameplayStatics.h"
 
 
 // Sets default values for this component's properties
@@ -38,6 +40,27 @@ void UTankAimingComponent_::TickComponent(float DeltaTime, ELevelTick TickType, 
 
 void UTankAimingComponent_::AimAt(FVector HitLocation, float LaunchSpeed)
 {
-	UE_LOG(LogTemp, Warning, TEXT("Firing at %f"), LaunchSpeed);
+	if (!Barrel) { return; }
 	
+	FVector OutLaunchVelocity;
+	FVector StartLocation = Barrel->GetSocketLocation(FName ("Projectile"));
+	if (UGameplayStatics::SuggestProjectileVelocity
+		(
+			this,
+			OutLaunchVelocity,
+			StartLocation,
+			HitLocation,
+			LaunchSpeed,
+			false,
+			0,
+			0,
+			ESuggestProjVelocityTraceOption::DoNotTrace
+			)
+		)
+	{
+		auto AimDirection = OutLaunchVelocity.GetSafeNormal();
+		auto TankName = GetOwner()->GetName();
+		UE_LOG(LogTemp, Warning, TEXT("%s Aiming at %s"), *TankName	, *AimDirection.ToString());
+	}
+	// If no solution found do nothing
 }
