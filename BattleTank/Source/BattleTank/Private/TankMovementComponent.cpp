0964a2a7f6ae0cf17dc55da2b999fab696d51296
Alexandre Santos <sasle57@gmail.com>
2018-09-18 19:33:14 -0300

    Aula 172

--- BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Copyright Danubio Inc.
 
 #include "TankMovementComponent.h"
 #include "TankTrack.h"
@@ -21,8 +21,6 @@ void UTankMovementComponent::RequestDirectMove(const FVector& MoveVelocity, bool
 
 	auto TurnRight = FVector::CrossProduct(TankForward, AIForwardIntention).Z;
 	IntendTurnRight(TurnRight);
-
-	// UE_LOG(LogTemp, Warning, TEXT("%s vectoring to %s"), *TankName, *MoveVelocityString)
 }
 
 void  UTankMovementComponent::IntendMoveForward(float Throw)
@@ -30,7 +28,6 @@ void  UTankMovementComponent::IntendMoveForward(float Throw)
 	if (!LeftTrack || !RightTrack) { return; }
 	LeftTrack->SetThrottle(Throw);
 	RightTrack->SetThrottle(Throw);
-	// TODO prevent double speed due to dual control use
 }
 
 void  UTankMovementComponent::IntendTurnRight(float Throw)
@@ -38,7 +35,6 @@ void  UTankMovementComponent::IntendTurnRight(float Throw)
 	if (!LeftTrack || !RightTrack) { return; }
 	LeftTrack->SetThrottle(Throw);
 	RightTrack->SetThrottle(-Throw);
-	// TODO prevent double speed due to dual control use
 }
 
 
